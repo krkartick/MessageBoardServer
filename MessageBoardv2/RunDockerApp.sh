77e5fff20ddda6e27a92572f011ca0c6f529b908
krkartick <krkartick@gmail.com>
2017-08-28 22:18:52 +0530

    Update RunDockerApp.sh

--- MessageBoardv2/RunDockerApp.sh
+++ MessageBoardv2/RunDockerApp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i -t -p 9900:9900 --name MessageBoardServerv2 -d go_mb_server_v2
+docker run -i -t -p 9900:9900 --name MessageBoardServerv2 -d krkartick/mbserverv2.project
